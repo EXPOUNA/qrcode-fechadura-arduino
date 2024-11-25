edc6fede8d3474456341f80ebe7193e21af17e73
Arthur <arthurnaocopia@gmail.com>
2024-11-24 23:01:51 -0300

    feat/função para verificar se o usuário existe e retornar apenas os dados necessários

---
+++ verificacao_qr.py
@@ -0,0 +1,10 @@
+from conexao_supabase import pesquisar_usuario
+
+def confirmar_usuario_qr(ra):
+    usuario = pesquisar_usuario(ra)
+        
+    if usuario:
+        usuario = {"RA": usuario["usu_ra"], "NOME": usuario["usu_nome"]}
+        return usuario
+    else:
+        return None
